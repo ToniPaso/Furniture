7d182cdf409e3b6a6789cba80c224ea2fa2d80f1
Toni Paso <156366416+ToniPaso@users.noreply.github.com>
2024-07-10 10:58:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,221 @@
+
+function changeImage1(x,image)
+{
+    if(x==1)
+    {
+        image.src = 'Garniture/Gaby(New)/1.jpg';
+    }
+    if (x==2)
+    {
+        image.src = 'Garniture/Gaby(New)/2.jpg';
+        
+    }
+}
+
+function changeImage2(x,image)
+{
+    if(x==1)
+    {
+        image.src = 'Garniture/Andrea(new)/2(2).jpg';
+    }
+    if (x==2)
+    {
+        image.src = 'Garniture/Andrea(new)/1.1(1).jpg';
+        
+    }
+}
+
+
+// Changing the box image! For third image Paris
+function changeImage3(x,image)
+{
+    if(x==1)
+    {
+        image.src = 'Garniture/Madrid/Madrid1-4.jpg';
+    }
+    if (x==2)
+    {
+        image.src = 'Garniture/Madrid/Madrid1-1.jpg';
+        
+    }
+}
+
+
+function changeImage4(x,image)
+{
+    if(x==1)
+    {
+        image.src = 'Garniture/Merkur XL(new)/2.jpg';
+    }
+    if (x==2)
+    {
+        image.src = 'Garniture/Merkur XL(new)/1.jpg';
+        
+    }
+}
+
+function changeImage5(x,image)
+{
+    if(x==1)
+    {
+        image.src = 'Garniture/Barcelona(new)/3.jpg';
+    }
+    if (x==2)
+    {
+        image.src = 'Garniture/Barcelona(new)/1.jpg';
+        
+    }
+}
+
+function changeImage6(x,image)
+{
+    if(x==1)
+    {                 
+        image.src = 'Garniture/Tommy Praktik/3.jpg';
+    }
+    if (x==2)
+    {
+        image.src = 'Garniture/Tommy Praktik/1.jpg';
+        
+    }
+}
+
+function changeImage7(x,image)
+{
+    if(x==1)
+    {
+        image.src = 'Garniture/Timo(new)/3.jpg';
+    }
+    if (x==2)
+    {
+        image.src = 'Garniture/Timo(new)/1.jpg';
+        
+    }
+}
+
+function changeImage8(x,image)
+{
+    if(x==1)
+    {
+        image.src = 'Garniture/Polly/Polly4.1.jpg';
+    }
+    if (x==2)
+    {
+        image.src = 'Garniture/Polly/Polly4.2.jpg';
+        
+    }
+}
+
+function changeImage9(x,image)
+{
+    if(x==1)
+    {
+        image.src = 'Garniture/Kosmik/Kosmik4.2.jpg';
+    }
+    if (x==2)
+    {
+        image.src = 'Garniture/Kosmik/Kosmik XL 5.jpg';
+        
+    }
+}
+
+function changeImage10(x,image)
+{
+    if(x==1)
+    {
+        image.src = 'Garniture/Lima duo/1.2.jpg';
+    }
+    if (x==2)
+    {
+        image.src = 'Garniture/Lima duo/1.1.jpg';
+        
+    }
+}
+
+
+function changeImage11(x,image)
+{
+    if(x==1)
+    {
+        image.src = 'Garniture/Roko Lux/0.jpg';
+    }
+    if (x==2)
+    {
+        image.src = 'Garniture/Roko Lux/roko front.jpg';
+        
+    }
+}
+
+
+function changeImage12(x,image)
+{
+    if(x==1)
+    {
+        image.src = 'Garniture/Hills/razvucen.jpg';
+    }
+    if (x==2)
+    {
+        image.src = 'Garniture/Hills/front.jpg';
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+function changeImage13(x,image)
+{
+    if(x==1)
+    {
+        image.src = 'Trosedi/Hills(new)/3.jpg';
+    }
+    if (x==2)
+    {
+        image.src = 'Trosedi/Hills(new)/1.jpg';
+        
+    }
+}
+
+function changeImage14(x,image)
+{
+    if(x==1)
+    {
+        image.src = 'Trosedi/Kongo/Kongo5.3.jpg';
+    }
+    if (x==2)
+    {
+        image.src = 'Trosedi/Kongo/Kongo5.1.jpg';
+        
+    }
+}
+
+function changeImage15(x,image)
+{
+    if(x==1)
+    {
+        image.src = 'Trosedi/Orion(blue)/Orion AAA_7269 copy.jpg';
+    }
+    if (x==2)
+    {
+        image.src = 'Trosedi/Orion(blue)/Orion AAA_7261 copy.jpg';
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
